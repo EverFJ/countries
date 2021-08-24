0c3c9f14b91987a8c2dac781b5637d51a400a30a
Francois Janson <jansonfrancois@gmail.com>
2021-08-24 14:37:46 +0200

    styling with bootstrap icons

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,13 +3,15 @@ import React from "react";
 class Card extends React.Component {
   render() {
     return (
-      <>
-        <img src={this.props.flag} alt="flag" width="300px" />
-        <h2 className="mt-4">Country: {this.props.name}</h2>
-        <h3 className="mt-4">Capital : {this.props.capital}</h3>
-        <h4 className="mt-4">Region : {this.props.region}</h4>
-        <h4 className="mt-4">Population : {this.props.population}</h4>
-      </>
+      <div className="card-block mt-1">
+        <img className="mt-3" src={this.props.flag} alt="flag" width="300px" />
+        <h2 className="mt-3">Country: {this.props.name}</h2>
+        <h3 className="mt-3">Capital : {this.props.capital}</h3>
+        <h4 className="mt-3">
+          <i class="bi bi-globe"></i>Region : {this.props.region}
+        </h4>
+        <h4 className="mt-3">Population : {this.props.population}</h4>
+      </div>
     );
   }
 }
